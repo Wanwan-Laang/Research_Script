d932bfefb146dce34d10b5beec2f5a0dd9d7cc68
Wanwan-Laang <wleng2@illinois.edu>
2025-01-30 01:25:10 -0600

    update every time

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,9 @@
 git status
+git add new-modified-file-here
+git commit -m "avoid error when use np.concatenate([]). save result in CSV file"
+git push 
+
+git status
 git add git-push.sh
-git commit -m "simple git push for I just too lazy to type git push"
+git commit -m "update every time"
 git push
