3696da0aeaa139373d91d3cd6da646b405ef7cc2
Wanwan-Laang <wleng2@illinois.edu>
2025-01-30 01:08:58 -0600

    fix: fix the random read error and make sure this script can read the lmp file correctly and correspondingly when converting to vasp files

--- convert_lmp2vasp_all.py
+++ convert_lmp2vasp_all.py
@@ -2,19 +2,18 @@ import os
 import dpdata
 
 atomic_species = ['F', 'Be', 'Li']
-atomic_masses = [56, 14, 28]
 
-current_dir = os.getcwd()
-lmp_files = [f for f in os.listdir(current_dir) if f.endswith('.lmp')]
+lmp_files = sorted([f for f in os.listdir() if f.endswith('.lmp')])
 
 for lmp_file in lmp_files:
     try:
         system = dpdata.System(lmp_file, fmt='lammps/lmp', type_map=atomic_species)
 
-        poscar_file = f"POSCAR_{os.path.splitext(lmp_file)[0]}"
+        file_number = os.path.splitext(lmp_file)[0]  
+        poscar_file = f"POSCAR_{int(file_number):02d}"  
 
         system.to('vasp/poscar', poscar_file)
-        print(f"Saved POSCAR to {poscar_file}")
+        print(f"Saved POSCAR as {poscar_file}")
 
     except Exception as e:
         print(f"Error processing {lmp_file}: {e}")
