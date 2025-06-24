bdff4dac78aeb53f8ce0f29db4100180c61f9c5d
Wanwan-Laang <wleng2@illinois.edu>
2025-06-23 21:47:57 -0500

    Add initial version of submission script
    Adds initial submission script for vacancy structures
    
    Introduces a Bash script to automate the setup and submission of VASP jobs for vacancy structure directories.
    
    The script copies necessary input files, modifies a parameter based on the directory name, and submits the job using `sbatch`.
    
    Facilitates streamlined processing of multiple vacancy configurations.

--- High_Entropy_Carbide_Vacancy_Ordering/01.submit-all-v1.sh
+++ High_Entropy_Carbide_Vacancy_Ordering/01.submit-all-v1.sh
@@ -1,1 +1,10 @@
-ii
+#!/bin/bash
+
+for dir in 01.vacancy_structures/1vac_C*/; do
+    vacancy=$(basename "$dir")
+    cp INCAR-relax INCAR-static KPOINTS POTCAR 02.run_vasp.sh "$dir"
+    cd "$dir" || exit
+    sed -i "s/6layer_10A/$vacancy/" 02.run_vasp.sh
+    sbatch 02.run_vasp.sh
+    cd - > /dev/null
+done
